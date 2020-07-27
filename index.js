7679da0fa02770da9fb0ed5a7b13aeb290addeda
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-07-27 14:49:57 +0530

    Add mongodb to genres routes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false })
+    .then(() => console.log('Connected with mongodb.....'))
+    .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
 
-
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, err=>{
+app.listen(port, err => {
     if (err) console.log('Some error occured......');
-    else console.log('App listening at port '+port);
+    else console.log('App listening at port ' + port);
 });
--- routes/genres.js
+++ routes/genres.js
@@ -1,70 +1,83 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const Joi = require('joi');
+const mongoose = require('mongoose');
 
-const genres = [
-    
-]
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+})
 
-mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser:true, useUnifiedTopology:true});
+const Genre = mongoose.model('Genre', genreSchema);
 
-router.get('/', (req, res)=>{
+router.get('/', async (req, res) => {
+    // if (!genres) return res.status(404).send("No genres in database!");
+    const genres = await Genre.find().sort('name');
     res.send(genres);
 })
 
-router.get('/:id', (req, res)=>{
+router.get('/:id', async (req, res) => {
     //check if genre with given id exists
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The genre with given id does not exist!');
-
-    res.send(genre);
-})
-
-router.post('/', (req, res)=>{
-    //validate the input name
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send('The genre name is not valid!');
-
-    const genre = {
-        id: genres.length +1,
-        name: req.body.name,
+    if (mongoose.isValidObjectId(req.params.id)) {
+        const genre = await Genre.findById(req.params.id);
+        if (!genre) return res.status(404).send('The genre with given id does not exist!');
+        res.send(genre);
     }
+    else return res.status(400).send('Invalid Id.');
 
-    genres.push(genre);
+})
+
+router.post('/', async (req, res) => {
+    //validate the input name
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let genre = new Genre({ name: req.body.name });
+
+    genre = await genre.save();
     res.send(genre);
 })
 
-router.put('/:id', (req, res)=>{
-     //check if genre with given id exists
-     const genre = genres.find(g => g.id === parseInt(req.params.id));
-     if(!genre) return res.status(404).send('The genre with given id does not exist!');
- 
-     //validate the input name
-    const {error} = validateGenre(req.body);
-    if(error) return res.status(400).send('The genre name is not valid!');
-    
-    //else put new genre 
-    genre.name = req.body.name;
-    res.send(genre);
-})
-
-router.delete('/:id', (req, res)=>{
+router.put('/:id', async (req, res) => {
     //check if genre with given id exists
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The genre with given id does not exist!');
+    if (mongoose.isValidObjectId(req.params.id)) {
+        const genre = await Genre.findById(req.params.id);
+        if (!genre) return res.status(404).send('The genre with given id does not exist!');
+        //validate the input name
+        const { error } = validateGenre(req.body);
+        if (error) return res.status(400).send('The genre name is not valid!');
+    
+        //else put new genre 
+        genre.name = req.body.name;
+        const result = await genre.save();
+        res.send(result);
+    }
+    else return res.status(400).send('Invalid Id.');
 
-    //if exists then delete
-    genres.splice(genres.indexOf(genre), 1);
-    res.send(genre);
+})
+
+router.delete('/:id', async (req, res) => {
+    //check if genre with given id exists
+    if(mongoose.isValidObjectId(req.params.id))
+    {
+        const genre = await Genre.findByIdAndRemove(req.params.id);
+        if (!genre) return res.status(404).send('The genre with given id does not exist!');
+        res.send(genre);
+    }
+    else return res.status(400).send('Invalid Id.');
+    
 })
 
 
-function validateGenre(genre){
+function validateGenre(genre) {
     const schema = Joi.object({
-        name: Joi.string().required(),
+        name: Joi.string().required().min(5).max(50),
     })
     return schema.validate(genre);
 }
 
-module.exports =  router;
+module.exports = router;
