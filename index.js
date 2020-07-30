9f7a0531ccc1cca0bdc89ea5eba4048d6455dcee
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-07-30 15:50:17 +0530

    Add users route

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 
-mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect('mongodb://localhost/vidly',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
     .then(() => console.log('Connected with mongodb.....'))
     .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, err => {
