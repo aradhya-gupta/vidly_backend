0e5fa59c2d6402b46e82df0fc5553cd52ade2a54
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-08-01 11:18:59 +0530

    Add login route as auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
@@ -26,6 +27,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, err => {
